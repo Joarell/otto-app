5534143f8ae6127c2bd22745a942a55d8f6bc59f
Joarell <josephjoarell@gmail.com>
2025-03-23 01:39:44 -0300

    feat (update): adds the root handler and improve the final return

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,17 @@ export default class Otto extends WorkerEntrypoint {
 	async fetch(request: Request) {
 		const appReq = new Request('http://app.ottocratesolver.com');
 		const url = new URL(request.url);
+		const checkIn = url.pathname.startsWith('/api/v1/login') || url.pathname.startsWith('/');
+		let backEnd
 
-		if (url.pathname.startsWith('/api/v1/login')) {
-			const backEnd = await this.env.back.fetch(request);
+		if (checkIn) {
+			backEnd = await this.env.back.fetch(request);
 
 			if (backEnd.ok) {
 				const app = await this.env.ASSETS.fetch(appReq);
 				return(new Response(app.body, { status: app.status, headers: backEnd.headers }));
 			};
-		};
-		return(await this.env.back.fetch(request));
+		}
+		return(backEnd ? backEnd: await this.env.back.fetch(request));
 	};
 };
