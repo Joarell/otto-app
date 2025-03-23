34219e0171204f3cdbb638c4111dbf98164d385e
Joarell <josephjoarell@gmail.com>
2025-03-22 22:39:16 -0300

    refactor (api)!: updates the route response

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-interface Env {
-	ASSETS: Fetcher;
-}
+import { WorkerEntrypoint } from 'cloudflare:workers';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
+export default class Otto extends WorkerEntrypoint {
+	async fetch(request: Request) {
 		const url = new URL(request.url);
+		if (url.pathname.startsWith('/api/'))
+			return new Response('Hello from Otto!');
+		return(await this.env.ASSETS.fetch(request));
+	};
 
-		switch (url.pathname) {
-			case '/':
-				return (new Response("Hi From Otto!"));
-			default :
-				return (new Response('Not found!', {status : 404}));
-		};
-	},
-} satisfies ExportedHandler<Env>;
+	get ottoApp() {
+		return( this.fetch(new Request('http://app.ottocratesolver.com')));
+	};
+};
