906bd27d3021720cd33d2a97e8976c3d67b7054a
Joarell <josephjoarell@gmail.com>
2025-03-23 01:33:57 -0300

    chore (app): set the worker behavior

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class Otto extends WorkerEntrypoint {
 	async fetch(request: Request) {
+		const appReq = new Request('http://app.ottocratesolver.com');
 		const url = new URL(request.url);
-		if (url.pathname.startsWith('/api/'))
-			return new Response('Hello from Otto!');
-		return(await this.env.ASSETS.fetch(request));
-	};
 
-	get ottoApp() {
-		return( this.fetch(new Request('http://app.ottocratesolver.com')));
+		if (url.pathname.startsWith('/api/v1/login')) {
+			const backEnd = await this.env.back.fetch(request);
+
+			if (backEnd.ok) {
+				const app = await this.env.ASSETS.fetch(appReq);
+				return(new Response(app.body, { status: app.status, headers: backEnd.headers }));
+			};
+		};
+		return(await this.env.back.fetch(request));
 	};
 };
