3b6e5019436839576af47d27d88c3e07c966222a
Joarell <josephjoarell@gmail.com>
2025-03-26 01:26:49 -0300

    refactor (status): updates the status code

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export default class Otto extends WorkerEntrypoint {
 			if (backEnd.ok) {
 				const app = await this.env.ASSETS.fetch(appReq);
 				return(new Response(
-					backEnd.body, { status: 202, headers: backEnd.headers }
+					backEnd.body, { status: backEnd.status, headers: backEnd.headers }
 				));
 			};
 		}
