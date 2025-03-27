8ff82d9a4be5643cc51f54a62d824e96f680b8d0
Joarell <josephjoarell@gmail.com>
2025-03-27 00:53:45 -0300

    fix (update): improves the worker response and wrangler version

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 
-export default class Otto extends WorkerEntrypoint {
+export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
-		const appReq =		new Request('http://app.ottocratesolver.com');
-		const url =			new URL(request.url);
-		const checkIn =		url.pathname.startsWith('/api/v1/login') || url.pathname.startsWith('/');
-		let backEnd
+		const url =		new URL(request.url);
 
-		if (checkIn) {
-			backEnd = await this.env.back.fetch(request);
-
-			console.log(backEnd.headers)
-			if (backEnd.ok || await backEnd.text() === 'ok') {
-				return(
-					url.pathname.startwWith('/') ? this.env.ASSETS.fetch(request)
-					new Response(backEnd.body, { status: backEnd.status, headers: backEnd.headers })
-				);
-			};
-		};
-		return(backEnd ? backEnd: await this.env.back.fetch(request));
+		switch(url.pathname) {
+			case '/':
+				console.log(await this.env.OTTO_USERS.get('Professor'));
+				return(await this.env.ASSETS.fetch(request));
+			default:
+				return(await this.env.back.fetch(request));
+		}
 	};
 };
