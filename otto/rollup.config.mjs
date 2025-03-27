ddf9cc339feada17d31fe250cd5273246c60ffa8
Joarell <josephjoarell@gmail.com>
2025-03-27 16:31:23 -0300

    chore (update): sets the api auth

--- otto/rollup.config.mjs
+++ otto/rollup.config.mjs
@@ -1,5 +1,8 @@
-// import { rollup } from 'rollup';
+import html from '@rollup/plugin-html';
 import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+import replace from '@rollup/plugin-replace';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
 	input: './app/main.mjs',
@@ -24,3 +27,53 @@ export default {
 	dir: "./app/",
 	assetFileNames: "./app/[name]-[hash][extname]"
 };
+
+// export default [
+// 	{
+// 		input: './app/main.mjs',
+// 		output: [
+// 			{
+// 				file: './app/main.min.mjs',
+// 				format: 'esm',
+// 				assetFileNames: './app/[name]-[hash][extname]',
+// 				preserverModules: true,
+// 				plugins: [terser({
+// 					format: 'cjs',
+// 					maxWorkers: 4,
+// 					keep_classnames: true,
+// 					ecma: 2023
+// 				})],
+// 			},
+// 		],
+// 		dir: "./app/",
+// 		assetFileNames: "./app/[name]-[hash][extname]"
+// 	},
+// 	{
+// 		input: 'app/index.html', // Your main HTML file
+// 		output: {
+// 			dir: 'dist', // Output directory
+// 			format: 'es', // or 'es' or other format
+// 			entryFileNames: '[name].mjs', // Output file name
+// 			assetFileNames: '[name].[ext]', // Asset file name
+// 		},
+// 		plugins: [
+// 			html({
+// 				input: 'src/index.html', // Your main HTML file
+// 				output: {
+// 					dir: 'dist', // Output directory
+// 					format: 'es', // or 'es' or other format
+// 					entryFileNames: '[name].js', // Output file name
+// 					assetFileNames: '[name].[ext]', // Asset file name
+// 				},
+// 			}),
+// 			css(), // Extracts CSS
+// 			nodeResolve(), // Resolves node modules
+// 			replace({
+// 				preventAssignment: true, // Prevent assignment to the replaced values
+// 				values: {
+// 					'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV), // Replace environment variables
+// 				},
+// 			}),
+// 		],
+// 	},
+// ];
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,21 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
 		const url =	new URL(request.url);
+		const ok =	['https://ottocratesovler.com', 'https://app.ottocratesovler.com', null];
 		let auth: Response;
 
+		console.log('origin', request.headers.get('origin'));
+		if(ok.includes(request.headers.get('origin')))
+			return(await this.env.ASSETS.fetch(request));
 		switch(url.pathname) {
 			case '/':
-				return(await this.env.ASSETS.fetch(request));
-				// auth = await this.env.back.fetch(request);
-				// console.log('ROOT', await auth.text())
-				// return(
-				// 	await auth.text() === 'ok' ?
-				// 		await this.env.ASSETS.fetch(request):
-				// 		new Response('Not Found!', { status: 404 })
-				// );
+				auth =		await this.env.back.fetch(request);
+				const msg = await auth.text();
+				console.log('ROOT', msg);
+				return(
+					msg === 'ok' ? await this.env.ASSETS.fetch(request):
+						new Response('Not Found!', { status: 404 })
+				);
 			case '/:name':
 				auth = await this.env.back.fetch(request);
 				const checkIn = await this.env.OTTO_USERS.get(await auth.text());
