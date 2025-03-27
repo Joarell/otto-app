e3e98efb57df74fa80e962dd11bf7fff0081dfad
Joarell <josephjoarell@gmail.com>
2025-03-27 10:43:58 -0300

    feat (root): adds the a params to the root

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
-		const url =		new URL(request.url);
+		const url =	new URL(request.url);
 
 		switch(url.pathname) {
-			case '/':
+			case '/:name':
 				console.log(await this.env.OTTO_USERS.get('Professor'));
 				return(await this.env.ASSETS.fetch(request));
 			default:
