3cbb013d93a6e1c5bfbf9fcb67308ffb09129d1d
Joarell <josephjoarell@gmail.com>
2025-03-27 14:13:53 -0300

    chore (upates): updates the wrangler and worker

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,28 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
 		const url =	new URL(request.url);
+		let auth: Response;
 
 		switch(url.pathname) {
-			case '/:name':
-				console.log(await this.env.OTTO_USERS.get('Professor'));
+			case '/':
 				return(await this.env.ASSETS.fetch(request));
+				// auth = await this.env.back.fetch(request);
+				// console.log('ROOT', await auth.text())
+				// return(
+				// 	await auth.text() === 'ok' ?
+				// 		await this.env.ASSETS.fetch(request):
+				// 		new Response('Not Found!', { status: 404 })
+				// );
+			case '/:name':
+				auth = await this.env.back.fetch(request);
+				const checkIn = await this.env.OTTO_USERS.get(await auth.text());
+				console.log('APP', checkIn);
+				return(
+					checkIn && checkIn !== null ?
+						await this.env.ASSETS.fetch(request): auth
+				);
 			default:
 				return(await this.env.back.fetch(request));
-		}
+		};
 	};
 };
