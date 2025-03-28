4c41bcc609dda09db0584c6ab484f67aa63bea6b
Joarell <josephjoarell@gmail.com>
2025-03-28 15:50:08 -0300

    feat (root): improves the root access

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,26 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
 		const url =	new URL(request.url);
-		const ok =	['https://app.ottocratesolver.com', null];
-		const origin = ok.includes(request.headers.get('origin'));
+		const ok =	[
+			'https://app.ottocratesolver.com/',
+			'https://ottocratesolver.com/',
+			'https://app.ottocratesolver.com',
+			'https://ottocratesolver.com'
+		];
+		const origin = ok.includes(request.headers.get('referer'));
 		const access = request.method === 'GET' && origin;
-		let auth: Response;
 
-		console.log("Header", request.headers);
+		console.log('Origin', origin);
 		if(request.method === 'OPTIONS')
 			return(await this.env.back.fetch(request));
+		if(origin && url.pathname === '/')
+			return(await this.env.ASSETS.fetch(request))
 		switch(url.pathname) {
 			case '/':
-				auth =		await this.env.back.fetch(request);
+				request.headers.forEach((value, key) => {
+					console.log(`${key}: ${value}`)
+				});
+				const auth = await this.env.back.fetch(request);
 				if (auth.ok) {
 					const APP = await this.env.ASSETS.fetch(request);
 					const newHeaders = new Headers(APP.headers)
@@ -24,19 +33,9 @@ export default class extends WorkerEntrypoint {
 					newHeaders.append('content-type', 'text/css; charset=utf-8');
 					newHeaders.append('content-type', 'text/javascript; charset=utf-8');
 					newHeaders.set('cache-control', 'max-age=28800, proxy-revalidate, immutable');
-					return(
-						new Response(APP.body, { status: 200, headers: newHeaders })
-					);
+					return(new Response(APP.body, { status: 200, headers: newHeaders }));
 				}
 				return(new Response('Not Found!', { status: 404 }));
-			case '/:name':
-				auth = await this.env.back.fetch(request);
-				const checkIn = await this.env.OTTO_USERS.get(await auth.text());
-				console.log('APP', checkIn);
-				return(
-					checkInmsg === 'ok' && checkIn !== null ?
-						await this.env.ASSETS.fetch(request): auth
-				);
 			default:
 				return(await this.env.back.fetch(request));
 		};
