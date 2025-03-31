33d029898d4c4f785a3746121c5d2cff302c0508
Joarell <josephjoarell@gmail.com>
2025-03-31 00:17:41 -0300

    chore (update): set the header file directives and worker requests

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,31 @@ export default class extends WorkerEntrypoint {
 		const name: string | null = url.searchParams.get('name');
 		const host = url.host === 'app.ottocratesolver.com' || url.host === 'ottocratesolver.com';
 
-		if(name) {
-			const checkIn = new Promise(async (resolve, reject) => {
-				const user: string | null = await this.env.OTTO_USERS.get(name);
-				console.log('NAME:', user);
-				user !== null ? resolve(user): reject("Empty");
-			});
-			const logged = await checkIn;
+		// if(name) {
+		// 	const checkIn = new Promise(async (resolve, reject) => {
+		// 		const user: string | null = await this.env.OTTO_USERS.get(name);
+		// 		console.log('NAME:', user);
+		// 		console.log('WWW', request.headers.get('Authorization'));
+		// 		user !== null ? resolve(user): reject("Empty");
+		// 	});
+		// 	const logged = await checkIn;
+		// 	return(
+		// 		logged !== "Empty" ?
+		// 			await this.env.ASSETS.fetch(app):
+		// 			new Response("Not found!", { status: 401 })
+		// 	);
+		// };
+
+		if(url.pathname.startsWith('/')) {
+			console.log('Header', request.headers);
+			const auth = request.headers.get('Authorization');
+			const back = await this.env.back.fetch(request)
+
+			console.log('HASHED', auth);
 			return(
-				logged !== "Empty" ?
-					await this.env.ASSETS.fetch(app):
-					new Response("Not found!", { status: 401 })
+				back.status === 200 ? await this.env.ASSETS.fetch(request): back
 			);
-		}
+		};
 
 		if(url.pathname.startsWith('/api/v1/login')) {
 			const login: Response = await this.env.back.fetch(request);
@@ -35,7 +47,7 @@ export default class extends WorkerEntrypoint {
 		else if(host) {
 			const auth: Response = await this.env.back.fetch(app);
 			return(auth.status === 200 ? await this.env.ASSETS.fetch(request): auth);
-		}
+		};
 		return(await this.env.back.fetch(request));
 	};
 };
