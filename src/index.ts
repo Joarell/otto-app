3a10153b4a9fcf4ef55ca4078065d7c7aea54d25
Joarell <josephjoarell@gmail.com>
2025-04-02 17:08:37 -0300

    feat (login): improves the logging headers response

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,22 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
-		const url =	new URL(request.url);
-		const app = new URL('https://app.ottocratesolver.com');
-		const host = url.host === 'app.ottocratesolver.com' || url.host === 'ottocratesolver.com';
+		const url =		new URL(request.url);
+		const app =		new URL('https://app.ottocratesolver.com')
 
+		// return(await this.env.ASSETS.fetch(request));
 		if(request.method === 'OPTIONS')
 			return(await this.env.back.fetch(request));
 
 		if(url.pathname.startsWith('/')) {
-			const auth = request.headers.get('Authorization');
 			const back: Response = await this.env.back.fetch(request)
-
-			console.log('BEARER', auth);
-			console.log('KV-USER', userKV);
+			console.log('BACK root:', back.status);
 			return(back.status === 200 ? await this.env.ASSETS.fetch(request): back);
-		};
-
-		if(host) {
-			const auth: Response = await this.env.back.fetch(app);
-			return(auth.status === 200 ? await this.env.ASSETS.fetch(request): auth);
-		};
+		}
+		// else if(url.pathname.startsWith('/api/v1/login')) {
+		// 	const logged: Response = await this.env.back.fetch(request);
+		// 	return(logged.status <= 308 ? await this.env.ASSETS.fetch(app): logged);
+		// };
 		return(await this.env.back.fetch(request));
 	};
 };
