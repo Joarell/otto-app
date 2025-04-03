8f27a0d8ae736853eba47806644b11a96d3f6889
Joarell <josephjoarell@gmail.com>
2025-04-03 20:44:07 -0300

    feat (api)!: set the app secure request to Otto-back worker

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,17 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
-		const url =		new URL(request.url);
-		const app =		new URL('https://app.ottocratesolver.com')
+		const url =	new URL(request.url);
+		const app =	new URL('https://app.ottocratesolver.com');
+		const { headers } = request;
 
-		// return(await this.env.ASSETS.fetch(request));
 		if(request.method === 'OPTIONS')
 			return(await this.env.back.fetch(request));
 
-		if(url.pathname.startsWith('/')) {
-			const back: Response = await this.env.back.fetch(request)
-			console.log('BACK root:', back.status);
-			return(back.status === 200 ? await this.env.ASSETS.fetch(request): back);
-		}
-		// else if(url.pathname.startsWith('/api/v1/login')) {
-		// 	const logged: Response = await this.env.back.fetch(request);
-		// 	return(logged.status <= 308 ? await this.env.ASSETS.fetch(app): logged);
-		// };
-		return(await this.env.back.fetch(request));
+		console.log(headers);
+		if(url.pathname.startsWith('/api/v1/login'))
+			return(await this.env.back.fetch(request));
+		const checkIn: Response = await this.env.back.fetch(app);
+		return(checkIn.ok ? await this.env.ASSETS.fetch(request): checkIn);
 	};
 };
