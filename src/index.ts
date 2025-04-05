e302f265b4019bd3eff0db58ec215aca8a24931a
Joarell <josephjoarell@gmail.com>
2025-04-05 01:22:24 -0300

    feat (cookies): adds the cookie setup header and update wrangler

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,40 @@ import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
-		const url =	new URL(request.url);
-		const app =	new URL('https://app.ottocratesolver.com');
-		const { headers } = request;
+		const url =		new URL(request.url);
+		const app =		new URL('https://app.ottocratesolver.com');
+		const user =	JSON.parse(await this.env.OTTO_USERS.get('Pass'));
 
-		if(request.method === 'OPTIONS')
-			return(await this.env.back.fetch(request));
+		if (request.method === 'OPTIONS')
+			return (await this.env.back.fetch(request));
 
-		console.log(headers);
-		if(url.pathname.startsWith('/api/v1/login'))
-			return(await this.env.back.fetch(request));
+		if (url.pathname.startsWith('/api/v1/login')) {
+			const auth: Response = await this.env.back.fetch(request);
+			await caches.default.put(url, auth.clone());
+			return (auth);
+		}
+
 		const checkIn: Response = await this.env.back.fetch(app);
-		return(checkIn.ok ? await this.env.ASSETS.fetch(request): checkIn);
+		checkIn.status === 200 ? await caches.default.put(
+			request.url, await this.env.ASSETS.fetch(request)
+		): false;
+		const asset: Response = await this.env.ASSETS.fetch(request);
+
+		if (user) {
+			const headers: Headers = new Headers(asset.headers);
+
+			headers.set('Authorization', `Bearer ${user.refTOken}`);;
+			headers.append('set-cookie', `__Secure-Name=${user.userName}; max-age=28800; HttpOnly; SameSite=Lax; Path=/; Secure; Domain=ottocratesolver.com`);
+			headers.append('set-cookie',`__Secure-RefToken=${user.refToken}; max-age=3600; HttpOnly; SameSite=Lax; Path=/; Secure; Domain=ottocratesolver.com`);
+			headers.append('set-cookie',`__Secure-Session-ID=${user.session}; max-age=28800; HttpOnly; SameSite=Lax; Path=/; Secure; Domain=ottocratesolver.com`,);
+			headers.append('set-cookie',`__Secure-Access=${user.access}; max-age=28800; HttpOnly; SameSite=Lax; Path=/; Secure; Domain=ottocratesolver.com`,);
+
+			const response = new Response(asset.body, {
+				status: asset.status,
+				headers
+			});
+			return(response);
+		};
+		return (checkIn.status === 200 ? asset: checkIn);
 	};
 };
