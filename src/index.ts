77a6d6341689f9d6aaafa34dc1efa952cb5a0c47
Joarell <josephjoarell@gmail.com>
2025-04-09 00:02:55 -0300

    docs (updates): improves the code order and rename the otto API method

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,6 @@ export default class extends WorkerEntrypoint {
 	async fetch(request: Request) {
 		const url =			new URL(request.url);
 		const authAccess =	await this.authCheck(request);
-		const routes: string[] = [
-			'/api/v1/boot/login',
-			'/api/v1/currencies',
-			'/api/v1/boot/login',
-			'/api/v1/shift/tokens',
-			'/api/v1/newEstimate',
-			'/api/v1/logout',
-			'/api/v1/estimates/:ref_id',
-			'/api/v1/update/estimates'
-		];
 
 		if (request.method === 'OPTIONS')
 			return (await this.env.back.fetch(request));
@@ -23,30 +13,30 @@ export default class extends WorkerEntrypoint {
 			const result: Response = await this.env.back.fetch(request);
 			return (result);
 		}
-		else if (routes.includes(url.pathname))
-			return(await this.ottoAPIRoutes(request));
+		else if (url.pathname.startsWith('/api/v1/'))
+			return(await this.ottoRoutesAPI(request));
 		return(await this.appAssets(request, authAccess));
 	};
 
 	/**
-	* @method reach the Otto API for DB queries.
-	* @param request - the client content for access and cookies.
+	* @method executes the authorization API consult.
+	* @param request - it has the cookies for authorization checkout.
 	*/
-	private async ottoAPIRoutes(request: Request): Promise<Response> {
-		const cookies =	Object.fromEntries(request.headers).cookie;
-		const headers =	new Headers();
+	private async authCheck(request: Request): Promise<Auth> {
+		const app =		new URL('https://app.ottocratesolver.com');
+		const cookies = Object.fromEntries(request.headers).cookie;
+		const headers = new Headers();
 
 		headers.set('Cookies', `${cookies}`);
-		const revert = request.method === "GET" ?
-			this.env.back.fetch(new Request(request.url, {
-				method: request.method,
-				headers,
-			})): this.env.back.fetch(new Request(request.url, {
-				method: request.method,
-				headers,
-				body: request.body
-			}));
-		return(await revert);
+		const auth =	new Request(app, {
+			method: request.method,
+			headers
+		});
+		const result = {
+			response: await this.env.back.fetch(auth),
+			headers,
+		}
+		return(result);
 	};
 
 	/**
@@ -69,23 +59,23 @@ export default class extends WorkerEntrypoint {
 	};
 
 	/**
-	* @method executes the authorization API consult.
-	* @param request - it has the cookies for authorization checkout.
+	* @method reach the Otto API for DB queries.
+	* @param request - the client content for access and cookies.
 	*/
-	private async authCheck(request: Request): Promise<Auth> {
-		const app =		new URL('https://app.ottocratesolver.com');
-		const cookies = Object.fromEntries(request.headers).cookie;
-		const headers = new Headers();
+	private async ottoRoutesAPI(request: Request): Promise<Response> {
+		const cookies =	Object.fromEntries(request.headers).cookie;
+		const headers =	new Headers();
 
 		headers.set('Cookies', `${cookies}`);
-		const auth =	new Request(app, {
-			method: request.method,
-			headers
-		});
-		const result = {
-			response: await this.env.back.fetch(auth),
-			headers,
-		}
-		return(result);
+		const revert = request.method === "GET" ?
+			this.env.back.fetch(new Request(request.url, {
+				method: request.method,
+				headers,
+			})): this.env.back.fetch(new Request(request.url, {
+				method: request.method,
+				headers,
+				body: request.body
+			}));
+		return(await revert);
 	};
 };
