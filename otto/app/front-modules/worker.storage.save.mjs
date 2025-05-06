02f455f1563875bd920739203fe1ef155f6440de
Joarell <josephjoarell@gmail.com>
2025-05-06 12:15:54 -0300

    feat (worker): adds a worker for saving new crate sizes

---
+++ otto/app/front-modules/worker.storage.save.mjs
@@ -0,0 +1,22 @@
+globalThis.onmessage = (estimate) => {
+	const dataName =	"Results";
+	const request =		globalThis.indexedDB.open(dataName);
+	const { data } =	estimate;
+
+	request.onerror = (event) => {
+		alert(`ERROR: ${event.target.errorCode}`);
+	}
+	request.onsuccess = async (event) => {
+		const db =			event.target.result;
+		const object =		db.transaction(dataName, "readwrite")
+			.objectStore(dataName);
+		const existsInIDB =	object.get(data.reference);
+
+		existsInIDB.onsuccess = async () => {
+			existsInIDB.result === undefined ? object.add(data):
+			await Promise.resolve(object.delete(existsInIDB.result.reference))
+				.then(object.add(data));
+			globalThis.postMessage('OK');
+		};
+	}
+};
