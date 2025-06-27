d3e74243d66cff5d51feb32cae5e774a86abd405
Joarell <josephjoarell@gmail.com>
2025-06-27 13:15:08 -0300

    refactor (Update): redefine the crate size definition and arts coordinates

--- otto/test/Crater.standard.test.mjs
+++ otto/test/Crater.standard.test.mjs
@@ -22,57 +22,55 @@ describe("The are test to Standard Crate solver.", () => {
 		assert.deepStrictEqual(current, expected);
 	});
 
-	// it("TEST-02: returns 3 crates as a result array with length of 6.", () => {
-	// 	const list = artParser(mock.standard1);
-	// 	const current = new CraterStandard(list, true, 5, false).crates.length;
-	// 	const expected = 2;
-	//
-	// 	assert.deepStrictEqual(current, expected);
-	// });
-	//
-	// it("TEST-03: returns 1 crate with 5 layers.", () => {
-	// 	const list =		artParser(mock.standard2);
-	// 	const test =		new CraterStandard(list, true, 4, false);
-	// 	const current =		new CraterStandard(list, true, 4, false).crates[1].works.length;
-	// 	const expected =	3;
-	//
-	// 	console.log(test.crates)
-	// 	console.log(test.crates[0][3])
-	// 	assert.deepStrictEqual(current, expected);
-	// });
+	it("TEST-02: returns 3 crates as a result array with length of 6.", () => {
+		const list =	artParser(mock.standard1);
+		const current = new CraterStandard(list, true, 5, false).crates.length / 2;
+		const expected = 2;
 
-	// it("TEST-04: returns 1 crates as result an array with length of 2.", () => {
-	// 	const list =		artParser(mock.standard3);
-	// 	const current =		new CraterStandard(list, true, 4, false).crates.length;
-	// 	const expected =	4
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-03: returns 1 crate with 5 layers.", () => {
+		const list =		artParser(mock.standard2);
+		const current =		new CraterStandard(list, true, 5, false).crates.length / 2;
+		const expected =	1;
+
+		assert.deepStrictEqual(current, expected);
+	});
 	//
-	// 	assert.deepStrictEqual(current, expected);
-	// });
-	//
-	// it("TEST-05: returns 1 crates as result an array length of 2.", () => {
-	// 	const list =		artParser(mock.standard4);
-	// 	const current =		new CraterStandard(list, true, 4, false).crates.length;
-	// 	const expected =	2;
-	//
-	// 	assert.deepStrictEqual(current, expected);
-	// });
-	//
-	// it("TEST-06: returns 3 crates as a result an array with length 6.", () => {
-	// 	const { sorted } =	mock.findTubesTest();
-	// 	const list =		artParser(sorted);
-	// 	const current =		new CraterStandard(list, true, 4, false).crates.length;
-	// 	const expected =	6;
-	//
-	// 	assert.deepStrictEqual(current, expected);
-	// });
-	//
+	it("TEST-04: returns 1 crates as result an array with length of 2.", () => {
+		const list =		artParser(mock.standard3);
+		const current =		new CraterStandard(list, true, 4, false).crates.length / 2;
+		const expected =	1
+
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-05: returns 1 crates as result an array length of 2.", () => {
+		const list =		artParser(mock.standard4);
+		const current =		new CraterStandard(list, true, 4, false).crates.length;
+		const expected =	2;
+
+		assert.deepStrictEqual(current, expected);
+	});
+
+	it("TEST-06: returns 2 crates as a result an array with length 4.", () => {
+		const { sorted } =	mock.findTubesTest();
+		const list =		artParser(sorted);
+		const current =		new CraterStandard(list, true, 4, false).crates.length;
+		const expected =	4;
+
+		console.log(current)
+		assert.deepStrictEqual(current, expected);
+	});
+
 	it("TEST-07: .", () => {
 		const newList =		artParser(mock.standard6);
 		const works =		new Arranger(newList);
 		const { sorted } =	works.list;
 		const size =		new CraterStandard(sorted, true, 4, false).crates[0];
 		const current =		[ size[0], size[1], size[2] ];
-		const expected =	[ 173, 45.5, 158 ];
+		const expected =	[ 173, 60.5, 150 ];
 
 		assert.deepStrictEqual(current, expected);
 	});
